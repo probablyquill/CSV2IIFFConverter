7ea21d5124b1a9462467d48a6f736e55f13aabc5
probablyquill <48251719+probablyquill@users.noreply.github.com>
2021-04-29 09:27:41 -0400

    TODO
    Add TODO

--- Program.cs
+++ Program.cs
@@ -17,3 +17,9 @@ namespace CSV2FLL
         
     }
 }
+
+//TODO:
+//1.) Add checks to make sure that the data is all correctly entered into the csv.
+//2.) Add checks to make sure that all dates entered into the CSV or webapp are reachable,
+//i.e. make sure that there are no entires like "2/30/2020."
+//3.) Make sure that the converter actually works -> can't do this one on my own.
